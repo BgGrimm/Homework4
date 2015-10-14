6a590f0395573ca5b6a0e817d99aacee58d70bc5
BgGrimm <numizmatika@abv.bg>
2015-10-15 02:49:58 +0300

    initial commit

---
+++ Problem 1. Sum of 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_1.Sum_of_3_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            int b = 4;
+            int c = 11;
+
+            Console.WriteLine(a + b + c);
+        }
+    }
+}
---
+++ Problem 2.Company Information/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class CompanyInfo
+{
+    static void Main()
+    {
+        string program = "program";
+        string user = "user";
+        Console.WriteLine("{0, -30}|{1, 30}", program, user);
+        Console.WriteLine("______________________________________________________________");
+        string name = "Company name:";
+        string softuni = "Software University";
+        Console.WriteLine("{0, -30}|{1, 30}", name, softuni);
+        string address = "Company address:";
+        string tintiava = "15-18 Tintyava, Sofia";
+        Console.WriteLine("{0, -30}|{1, 30}", address, tintiava);
+        string phone = "Phone number:";
+        string number = "+359 899 55 55 92";
+        Console.WriteLine("{0, -30}|{1, 30}", phone, number);
+        string fax = "Fax number:";
+        string fnumber = "";
+        Console.WriteLine("{0, -30}|{1, 30}", fax, fnumber);
+        string web = "Web site:";
+        string site = "http://softuni.bg";
+        Console.WriteLine("{0, -30}|{1, 30}", web, site);
+        string first = "Manager first name:";
+        string svetlin = "Svetlin";
+        Console.WriteLine("{0, -30}|{1, 30}", first, svetlin);
+        string last = "Manager last name:";
+        string nakov = "Nakov";
+        Console.WriteLine("{0, -30}|{1, 30}", last, nakov);
+        string age = "Manager age:";
+        string years = "25";
+        Console.WriteLine("{0, -30}|{1, 30}", age, years);
+        string telephone = "Manager phone:";
+        string phnumber = "+359 2 981 981";
+        Console.WriteLine("{0, -30}|{1, 30}", telephone, phnumber);
+        Console.WriteLine("______________________________________________________________");
+        Console.WriteLine("{0}", softuni);
+        Console.WriteLine("Address: {0}", tintiava);
+        Console.WriteLine("Telephone: {0}", number);
+        Console.WriteLine("Fax: {0}", fnumber);
+        Console.WriteLine("Web site: {0}", site);
+        Console.WriteLine("Manager: {0} {1} (age: {2}, tel. {3})", svetlin, nakov, years, phnumber);
+        Console.WriteLine("______________________________________________________________");
+    }
+}
